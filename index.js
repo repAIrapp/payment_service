31da7c3543e46c583fb4913fa96949fa943790a8
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-07-06 21:45:09 +0200

    add: metrics route config

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const paymentRoutes = require("./src/routes/paymentRoute");
-const webhookRoutes = require("./src/routes/webhookroute"); // ✅ Ajout de ta route webhook
-
+const webhookRoutes = require("./src/routes/webhookroute"); 
+const client = require("prom-client");
 dotenv.config();
 const app = express();
+
+const register = new client.Registry();
+// Crée une métrique de type Counter
+const paymentRequestsCounter = new client.Counter({
+  name: "payment_requests_total",
+  help: "Nombre total de requêtes sur le service payment",
+  labelNames: ["method", "route", "status"]
+});
+
+// Enregistre la métrique dans le registre
+register.registerMetric(paymentRequestsCounter);
+
+// Collecte les métriques système par défaut
+client.collectDefaultMetrics({ register });
+
+// Middleware pour enregistrer chaque requête
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    paymentRequestsCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode
+    });
+  });
+  next();
+});
 
 // 🟠 Le webhook Stripe a besoin du body brut — donc il doit être défini AVANT express.json()
 app.use("/api/webhook", webhookRoutes); // ✅ Branche d'abord le webhook ici
@@ -16,7 +42,18 @@ app.use(express.json()); // 🔴 À ne PAS mettre avant le webhook
 
 // ✅ Routes de paiement
 app.use("/api/payments", paymentRoutes);
-
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
 app.listen(process.env.PORT, () =>
   console.log(` Paiement service running on http://localhost:${process.env.PORT}`)
 );
+const metricsApp = express();
+metricsApp.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.send(await register.metrics());
+});
+metricsApp.listen(9103, () => {
+  console.log("📊 payment service metrics exposed on http://localhost:9103/metrics");
+});
