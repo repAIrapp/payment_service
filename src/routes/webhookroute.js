955d009996cafce1b48aafa507ca6bfe60240e41
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-09-14 22:37:09 +0200

    fix:prod

--- src/routes/webhookroute.js
+++ src/routes/webhookroute.js
@@ -1,13 +1,25 @@
+// const express = require("express");
+// const router = express.Router();
+// const stripeWebhook = require("../webhook/stripeWebhook");
+// const bodyParser = require("body-parser");
+
+// // router.post("/", express.raw({ type: "application/json" }), stripeWebhook);
+// router.post(
+//   "/",
+//   bodyParser.raw({ type: "application/json" }),
+//   stripeWebhook
+// );
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = express.Router();
 const stripeWebhook = require("../webhook/stripeWebhook");
-const bodyParser = require("body-parser");
 
-// router.post("/", express.raw({ type: "application/json" }), stripeWebhook);
-router.post(
-  "/",
-  bodyParser.raw({ type: "application/json" }),
-  stripeWebhook
-);
+// ⚠️ Stripe exige le body RAW pour vérifier la signature.
+// On monte express.raw uniquement sur CETTE route.
+router.post("/", express.raw({ type: "application/json" }), stripeWebhook);
 
 module.exports = router;
